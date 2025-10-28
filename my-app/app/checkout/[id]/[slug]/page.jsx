de413917c1e01213f4ad48fa895b6673d82a714f
Limon <mahammudhassanlimon@gmail.com>
2025-10-28 18:39:57 +0600

    done full

--- my-app/app/checkout/[id]/[slug]/page.jsx
+++ my-app/app/checkout/[id]/[slug]/page.jsx
@@ -16,7 +16,6 @@ const page = () => {
   let [SummeryData, setSummeryData] = useState({});
   let [saveInfo, setSaveInfo] = useState(false);
 
-  
   let handlePaymentChange = paymentMethod => {
     setSelectpayment(paymentMethod);
   };
@@ -246,7 +245,7 @@ const page = () => {
                 <h4 className="text-[26px] font-noto-bengali font-bold text-[#000] mb-[30px]">
                   আপনার তথ্য দিন
                 </h4>
-                <div className="mobile:w-[270px] tablet:w-[510px] laptop:w-[570px] computer:w-[570px] mb-[20px] ">
+                <div className="mobile:w-full tablet:w-full laptop:w-full computer:w-[570px] mb-[20px] ">
                   <label
                     className="text-[16px] font-noto-bengali font-medium text-[#000] mb-[10px]"
                     htmlFor="text"
@@ -263,7 +262,7 @@ const page = () => {
                     placeholder="আপনার নাম..."
                   />
                 </div>
-                <div className="mobile:w-[270px] tablet:w-[510px] laptop:w-[570px] computer:w-[570px] mb-[20px] ">
+                <div className="mobile:w-full tablet:w-full laptop:w-full computer:w-[570px] mb-[20px] ">
                   <label
                     className="text-[16px] font-noto-bengali font-medium text-[#000] mb-[10px]"
                     htmlFor="text"
@@ -280,7 +279,7 @@ const page = () => {
                     placeholder="আপনার পূর্ণ ঠিকানা..."
                   />
                 </div>
-                <div className="mobile:w-[270px] tablet:w-[510px] laptop:w-[570px] computer:w-[570px] mb-[20px]">
+                <div className="mobile:w-full tablet:w-full laptop:w-full computer:w-[570px] mb-[20px]">
                   <label
                     className="text-[16px] font-noto-bengali font-medium text-[#000] mb-[10px]"
                     htmlFor="text"
@@ -341,12 +340,12 @@ const page = () => {
                     (+) {SummeryData.subTotal || 0}৳
                   </h4>
                 </div>
-                {SummeryData.disCountPrice && (
+                {SummeryData.disCountPrice > 0 && (
                   <div className="flex items-center justify-between mb-[20px] border-t border-[#000]/30 ">
                     <h4 className="text-[16px] font-bold font-nunito text-gray-500 mt-[10px]">
-                      Discount ({SummeryData.showingDiscount || 0} % ছাড়)
+                      Discount ({SummeryData.showingDiscount} % ছাড়)
                     </h4>
-                    <h4 className="mt-[10px] text-[16px] font-bold font-noto-bengali text-gray-600">
+                    <h4 className="mt-[10px] text-[16px] font-bold font-noto-bengali text-red-500">
                       (-) {SummeryData.disCountPrice || 0}৳
                     </h4>
                   </div>
@@ -389,7 +388,7 @@ const page = () => {
                 </h4>
                 <button
                   onClick={handleSubmit}
-                  className="mobile:text-[20px] tablet:text-[28px] laptop:text-[28px] computer:text-[28px] font-bold font-noto-bengali text-white bg-[#C67D09] py-[12px] mobile:px-[82px] tablet:px-[185px] laptop:px-[145px] computer:px-[145px] cursor-pointer rounded-[4px] mt-[30px]"
+                  className="mobile:text-[20px] tablet:text-[28px] laptop:text-[28px] computer:text-[28px] font-bold font-noto-bengali text-white bg-[#C67D09] w-full h-[60px] cursor-pointer rounded-[4px] mt-[30px]"
                 >
                   অর্ডার করুন
                 </button>
