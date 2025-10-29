a523c2a86fc8ce52fb7029681a67c657c21f2a0b
Limon <mahammudhassanlimon@gmail.com>
2025-10-30 04:44:06 +0600

    dome

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,15 +1,7 @@
 import Homepage from '../Componets/Homepage/Homepage';
-export const metadata = {
-  title: 'Deluxe X Deal BD - Hardware & Power Tools Shop',
-  description:
-    'Buy premium drill machines, power tools, and hardware equipment from Deluxe X Deal BD — trusted by professionals across Bangladesh.',
-};
-const page = async () => {
-  return (
-    <>
-      <Homepage />
-    </>
-  );
+
+const Page = async () => {
+  return <Homepage />;
 };
 
-export default page;
+export default Page;
