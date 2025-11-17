e5d08b5c95a783e45100e7f3f1459a2afed3a603
Limon <mahammudhassanlimon@gmail.com>
2025-11-17 16:06:18 +0600

    done

--- my-app/app/ClientProvider.jsx
+++ my-app/app/ClientProvider.jsx
@@ -2,8 +2,17 @@
 import { SnackbarProvider } from 'notistack';
 import { SocketContext } from '../utills/socketContext';
 import socket from '../utills/socket';
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function ClientProvider({ children }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof fbq === 'function') {
+      fbq('track', 'PageView');
+    }
+  }, [pathname]);
   return (
     <SocketContext.Provider value={socket}>
       <SnackbarProvider
--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { Nunito, Noto_Sans_Bengali } from 'next/font/google';
 import './globals.css';
 import Navber from '../Componets/Navber';
@@ -52,6 +53,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${nunito.variable} ${notoBengali.variable}`}>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
         <Navber />
         <Navber_1 />
         <Navber_2 />
