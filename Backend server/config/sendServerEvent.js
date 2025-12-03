113c9c6ae7b019ae7233b9af4beb7c14ae8969c6
Limon <mahammudhassanlimon@gmail.com>
2025-12-03 22:29:15 +0600

    pixel done

---
+++ Backend server/config/sendServerEvent.js
@@ -0,0 +1,41 @@
+
+export const sendServerEvent = async (eventName, eventData = {}) => {
+  try {
+    const pixelId = process.env.META_PIXEL_ID;
+    const accessToken = process.env.META_ACCESS_TOKEN;
+
+    if (!pixelId || !accessToken) {
+      throw new Error('Pixel ID or Access Token missing from env');
+    }
+
+    const url = `https://graph.facebook.com/v13.0/${pixelId}/events?access_token=${accessToken}`;
+
+    const payload = {
+      data: [
+        {
+          event_name: eventName,
+          event_time: Math.floor(Date.now() / 1000),
+          action_source: 'server',
+          user_data: {
+            client_ip_address: eventData.ip,
+            client_user_agent: eventData.ua,
+          },
+          custom_data: eventData.custom_data || {},
+        },
+      ],
+    };
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    });
+
+    const data = await res.json();
+    console.log('SERVER SIDE EVENT SENT:', data);
+
+    return data;
+  } catch (err) {
+    console.error('Meta Pixel Server Error:', err);
+  }
+};
